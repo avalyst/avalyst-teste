da4411789192e7c21b9a73d39bb1ff719d1110a3
Charles Cavalcante <e-mail@charles.art.br>
2020-12-23 09:31:22 -0300

    Contact.php

---
+++ Services/Contact.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Api\Services;
+
+use Api\Validators\ContactValidator;
+use Api\Repositories\Contact;
+use Respect\Validation\Validator as v;
+
+class ContactService extends Service
+{
+    public function post($data)
+    {
+        $validator = new ContactValidator();
+
+        if($validator->validate($data) === false)
+        {
+            return $this->error($validator->errors, 200);
+        }
+
+        $contactModel = array(
+            'name' => trim($data['name']),
+            'email' => trim($data['email']),
+            'phone' => trim($data['phone']),
+            'message' => trim($data['message'])
+        );
+
+        try
+        {
+            $contact = Contact::create($contactModel);
+
+            $to = $this->settings;
+
+            $emailService = $this->container->get('EmailService');
+
+            $message = str_replace("\n", "<br>", $contact->message);
+            $data = date("d/m/Y") . ' &agrave;s ' . date("H:i");
+
+            $body = "<b>Nome:</b> {$contact->name}<br>
+                     <b>E-mail:</b> {$contact->email}<br>
+                     <b>Telefone:</b> {$contact->phone}<br>
+                     <b>Mensagem:</b> <br><br>{$message}<br><br><hr>
+                     <i>Mensagem enviada via Fale Conosco do site Avalyst em $data</i>";
+
+            if ($emailService->send($contact->email, $contact->name, $to, '', 'Fale Conosco', $body))
+            {
+                $this->logger->info('E-mail de contato enviado para '. implode(', ', $to));
+
+                return $this->ok();
+            }
+        }
+        catch (\Exception $e)
+        {
+            $this->logger->error('Erro ao cadastrar o contato', array($e));
+        }
+
+        if (empty($contact))
+        {
+            $msg = "Ocorreu um erro ao enviar a mensagem";
+
+            $this->logger->warning($msg);
+
+            return $this->error($msg);
+        }
+
+        return $this->ok();
+    }
+}
+
